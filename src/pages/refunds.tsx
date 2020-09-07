4703736d2afc165f861819d2da54a192b45428d2
Simply Digital <68376859+DigitalSimply@users.noreply.github.com>
2020-09-07 01:14:13 +0000

    Update refunds.tsx

--- src/pages/refunds.tsx
+++ src/pages/refunds.tsx
@@ -27,31 +27,31 @@ const Refunds = () => (
 
 			<section className="section section--padding">
 				<Preamble light title="Refunds">
-					Since our website is offering intangible goods refunds are
+					Since our website is offering intangible goods and services, refunds are
 					honored for the following reasons:
 					<br />
 					<br />
 					<strong>
-						* non-delivery of the product: in case we fail to
-						deliver the purchased goods you are entitled for a full
-						refund
-						<br />* service issues: in case our service is not
-						functioning as advertised you are entitled for a full
-						refund
+						* Failure To Deliver: In the event we fail to
+						deliver the purchased goods, you are entitled for a full
+						refund.
+						<br />* Vendor Side Connectivity Issue's: In the event our
+						service fails in its core functioning as advertised, you are entitled
+						to a full refund.
 					</strong>
 					<br />
 					<br />
-					Please note that we do not bear any responsibility and
-					therefore we do not satisfy any refund/return/exchange
+					Please note that we do not bear any usage responsibility.
+					Therefor we do not honor any refund/return/exchange
 					requests based on incompatibility of our products with some
 					third-party software (plug-ins, add-ons, modules, search
-					engines, scripts, extensions etc). We don't guarantee that
+					engines, scripts, extensions etc). We do not guarantee that
 					our products are fully compatible with any third-party
 					programs and we do not provide support for third-party
-					applications.
+					applications directly.
 					<br />
 					<br />
-					Our Technical Support Team is always eager to assist you and
+					Our Technical Support Team is always available to assist you and
 					deliver highly professional support in a timely manner.
 				</Preamble>
 
@@ -60,7 +60,7 @@ const Refunds = () => (
 					<ul>
 						<li>GSA</li>
 						<li>Xrumer</li>
-						<li>Torrent downloading/uploading</li>
+						<li>Torrenting</li>
 						<li>Scrapebox</li>
 					</ul>
 				</Preamble>
